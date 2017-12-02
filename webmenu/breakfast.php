6a01c08c5dd02f2d6e9bb4cc02049539ee7a455a
Ben Miller <bborkmiller@gmail.com>
2017-12-02 16:08:45 -0500

    Functionalized, improved archiving
    Moved some tasks into external functions and added some logic to the archiving function to reduce churn

--- webmenu/breakfast.php
+++ webmenu/breakfast.php
@@ -1,74 +1,9 @@
 <?php
 
-/* Open the menu file */
-$menu_file = "breakfast.txt";
-if (!file_exists($menu_file)) {
-	die("*** Menu file not found ***\r\n");
-} else {
-	$handle = fopen($menu_file, "r");
-}
+include 'menu_func.php';
+date_default_timezone_set("America/Detroit");
 
-// We'll store the menu data in this array
-$menu_body = array();
-while(! feof($handle))
-	{
-	// ltrim to just handle possible extra whitespace at the beginning
-	// rtrim to strip off the line feed at the end	
-	$line = ltrim(rtrim(fgets($handle)));
-	
-	// Check for blank lines. Don't do anything with those
-	if (strlen($line) > 0) {
-		// We'll put info for the new line in here
-		$new_line = array();
-		// Split line into words
-		$words = explode(" ", $line);
-	
-		// Figure out what kind of line we're looking at
-		switch ($words[0]) {
-			case "#"; // H1 header
-				$new_line["tag"] = "h1";
-				break;
-			case "##"; // H2 header
-				$new_line["tag"] = "h2";
-				break;
-			case "---"; // Column break
-				$new_line["tag"] = "div";
-				break;
-			default;
-				// Check for an image
-				if (strpos($words[0], ".jpg") !== false || strpos($words[0], ".png") !== false) {
-					$new_line["tag"] = "img";
-					} else {
-					$new_line["tag"] = "p";
-					}
-			} // end switch
-	
-		// Set the opening tag and the rest of the line
-		switch ($new_line["tag"]) {
-			case "p";
-				$new_line["content"] = ucfirst(ltrim($line));
-				break;
-			case "div";
-				$new_line["content"] = "</div>
-
-<div class=\"section\">";
-				break;
-			case "img";
-				$new_line["content"] = $line;
-				break;
-			default;
-				$trim_line = ltrim(rtrim(strstr($line, " ")));
-				//echo ucwords($trim_line) . "</" . $line_type . ">";
-				$new_line["content"] = ucwords($trim_line);
-		}
-		
-		$menu_body[] = $new_line;
-	} // end if
-} // end while
-
-fclose($handle);
-
-// These are the headers/footers for our two HTML files
+// These are the headers/footers for our HTML files
 // The display menu header
 $display_header = "<!DOCTYPE html>
 <html>
@@ -93,28 +28,40 @@ $display_footer = "</div>
 </body>
 </html>";
 
+/* Open the menu file */
+$menu_file = "breakfast.txt";
+if (!file_exists($menu_file)) {
+	die("*** Menu file not found ***\r\n");
+} else {
+	$handle = fopen($menu_file, "r");
+}
+
+// Read the menu file and load it into an array
+$menu_body = load_menu($handle);
+
+fclose($handle);
+
 // Write the website menu file
-// $web_menu = "";
-// foreach ( $menu_body as $menu_item ) {
-// 	// Omit the div and img
-// 	$skip_tags = array("div", "img");
-// 	if ( ! in_array( $menu_item["tag"], $skip_tags) ) {
-// 		$web_menu .= "\t<".$menu_item["tag"].">";
-// 		$web_menu .= $menu_item["content"];
-// 		$web_menu .= "</".$menu_item["tag"].">";
-// 		}
-// 	$web_menu .= "\n";
-// }
-// 
-// $handle = fopen('web_menu.html', 'w');
-// fwrite($handle, $web_menu);
-// fclose($handle);
+$web_menu = "";
+foreach ( $menu_body as $menu_item ) {
+	// Omit the div and img
+	$skip_tags = array("div", "img");
+	if ( ! in_array( $menu_item["tag"], $skip_tags) ) {
+		$web_menu .= "\t<".$menu_item["tag"].">";
+		$web_menu .= $menu_item["content"];
+		$web_menu .= "</".$menu_item["tag"].">";
+		}
+	$web_menu .= "\n";
+}
+
+$handle = fopen('breakfast_menu.html', 'w');
+fwrite($handle, $web_menu);
+fclose($handle);
 
 // Generate the display menu
 $display_menu = "";
 $display_menu .= $display_header;
 foreach ( $menu_body as $menu_item ) {
-	
 	switch ($menu_item["tag"]) {
 		case "div"; // do nothing
 			break;
@@ -138,16 +85,10 @@ foreach ( $menu_body as $menu_item ) {
 }
 $display_menu .= $display_footer;
 
+// Write out the menu to the browser
 echo $display_menu;
 
-/* Archive the menu file */
-date_default_timezone_set("America/Detroit");
-$datetime = date("Ymd-Gi");
-
-$newfile = "menu_archive/breakfast_" . $datetime . '.txt';
-
-if (!copy($menu_file, $newfile)) {
-    echo "** Failed to archive menu...";
-}
+// Archive the menu file
+archive_menu($menu_file, "breakfast");
 
 ?>
--- webmenu/displaymenu.php
+++ webmenu/displaymenu.php
@@ -1,73 +1,10 @@
 <?php
 
-/* Open the menu file */
-if (!file_exists("text_menu.txt")) {
-	die("*** Menu file not found ***\r\n");
-} else {
-	$handle = fopen("text_menu.txt", "r");
-}
+include 'menu_func.php';
 
-// We'll store the menu data in this array
-$menu_body = array();
-while(! feof($handle))
-	{
-	// ltrim to just handle possible extra whitespace at the beginning
-	// rtrim to strip off the line feed at the end	
-	$line = ltrim(rtrim(fgets($handle)));
-	
-	// Check for blank lines. Don't do anything with those
-	if (strlen($line) > 0) {
-		// We'll put info for the new line in here
-		$new_line = array();
-		// Split line into words
-		$words = explode(" ", $line);
-	
-		// Figure out what kind of line we're looking at
-		switch ($words[0]) {
-			case "#"; // H1 header
-				$new_line["tag"] = "h1";
-				break;
-			case "##"; // H2 header
-				$new_line["tag"] = "h2";
-				break;
-			case "---"; // Column break
-				$new_line["tag"] = "div";
-				break;
-			default;
-				// Check for an image
-				if (strpos($words[0], ".jpg") !== false || strpos($words[0], ".png") !== false) {
-					$new_line["tag"] = "img";
-					} else {
-					$new_line["tag"] = "p";
-					}
-			} // end switch
-	
-		// Set the opening tag and the rest of the line
-		switch ($new_line["tag"]) {
-			case "p";
-				$new_line["content"] = ucfirst(ltrim($line));
-				break;
-			case "div";
-				$new_line["content"] = "</div>
+date_default_timezone_set("America/Detroit");
 
-<div class=\"section\">";
-				break;
-			case "img";
-				$new_line["content"] = $line;
-				break;
-			default;
-				$trim_line = ltrim(rtrim(strstr($line, " ")));
-				//echo ucwords($trim_line) . "</" . $line_type . ">";
-				$new_line["content"] = ucwords($trim_line);
-		}
-		
-		$menu_body[] = $new_line;
-	} // end if
-} // end while
-
-fclose($handle);
-
-// These are the headers/footers for our two HTML files
+// These are the headers/footers for our HTML files
 // The display menu header
 $display_header = "<!DOCTYPE html>
 <html>
@@ -92,6 +29,19 @@ $display_footer = "</div>
 </body>
 </html>";
 
+/* Open the menu file */
+$menu_file = "text_menu.txt";
+if (!file_exists($menu_file)) {
+	die("*** Menu file not found ***\r\n");
+} else {
+	$handle = fopen($menu_file, "r");
+}
+
+// Read the menu file and load it into an array
+$menu_body = load_menu($handle);
+
+fclose($handle);
+
 // Write the website menu file
 $web_menu = "";
 foreach ( $menu_body as $menu_item ) {
@@ -113,7 +63,6 @@ fclose($handle);
 $display_menu = "";
 $display_menu .= $display_header;
 foreach ( $menu_body as $menu_item ) {
-	
 	switch ($menu_item["tag"]) {
 		case "div"; // do nothing
 			break;
@@ -137,17 +86,9 @@ foreach ( $menu_body as $menu_item ) {
 }
 $display_menu .= $display_footer;
 
+// Write out the menu to the browser
 echo $display_menu;
 
-/* Archive the menu file */
-date_default_timezone_set("America/Detroit");
-$datetime = date("Ymd-Gi");
-
-$file = "text_menu.txt";
-$newfile = "menu_archive/menu_" . $datetime . '.txt';
-
-if (!copy($file, $newfile)) {
-    echo "** Failed to archive menu...";
-}
-
+// Archive the menu file
+archive_menu($menu_file, "menu");
 ?>
